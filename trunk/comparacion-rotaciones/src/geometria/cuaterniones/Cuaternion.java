5821646e5947c9237caf41ed181eb44804de92d1
gabriel.tandil <gabriel.tandil@5db0f103-cdb5-6675-4ab8-29be0aa4deab>
2011-07-12 23:03:26 +0000

    avances, mayormente rotacion esferica

--- trunk/comparacion-rotaciones/src/geometria/cuaterniones/Cuaternion.java
+++ trunk/comparacion-rotaciones/src/geometria/cuaterniones/Cuaternion.java
@@ -5,67 +5,58 @@ import geometria.Vector;
 import geometria.esferica.CoordenadaEsferica;
 import geometria.esferica.RotacionEsferica;
 
-public class Cuaternion implements Rotacion, Vector
-{
-	private double	x;
-	private double	y;
-	private double	z;
-	private double	angulo;
+public class Cuaternion implements Rotacion, Vector {
+	private double x;
+	private double y;
+	private double z;
+	private double angulo;
 
-	public Cuaternion(double x, double y, double z, double angulo)
-	{
+	public Cuaternion(double x, double y, double z, double angulo) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
 		this.angulo = angulo;
 	}
 
-	public Cuaternion(double x, double y, double z)
-	{
+	public Cuaternion(double x, double y, double z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
 		this.angulo = 0;
 	}
 
-	public CoordenadaEsferica aEsferica()
-	{
+	public CoordenadaEsferica aEsferica() {
 		double modulo = getModulo();
 		double phi = Math.acos(z / modulo);
 		double lambda = Math.acos(x / (modulo * Math.sin(phi)));
 		return new CoordenadaEsferica(lambda, phi, modulo);
 	}
 
-	public double getAngulo()
-	{
+	public double getAngulo() {
 		return angulo;
 	}
 
-	public double getX()
-	{
+	public double getX() {
 		return x;
 	}
 
-	public double getY()
-	{
+	public double getY() {
 		return y;
 	}
 
-	public double getZ()
-	{
+	public double getZ() {
 		return z;
 	}
 
 	@Override
-	public Vector rotar(Vector v)
-	{
+	public Vector rotar(Vector v) {
 		Cuaternion cuaternion = null;
-		if (v instanceof Cuaternion)
-		{
+		if (v instanceof Cuaternion) {
+			System.out.println("rotar cuaternion dato cuaternion");
 			cuaternion = (Cuaternion) v;
 		}
-		if (v instanceof CoordenadaEsferica)
-		{
+		if (v instanceof CoordenadaEsferica) {
+			System.out.println("rotar cuaternion dato esferica");
 			cuaternion = ((CoordenadaEsferica) v).aCuaternion();
 		}
 
@@ -73,70 +64,60 @@ public class Cuaternion implements Rotacion, Vector
 
 	}
 
-	public Cuaternion rotar(Cuaternion v)
-	{
+	public Cuaternion rotar(Cuaternion v) {
 		Cuaternion rotacion = aRotacion();
 		Cuaternion rotado = rotacion.mult(v).mult(rotacion.conjugado());
 		return rotado;
 	}
 
-	public Cuaternion aRotacion()
-	{
+	public Cuaternion aRotacion() {
 		double anguloSobre2 = angulo / 2;
 		double senoAnguloSobre2 = Math.sin(anguloSobre2);
-		return new Cuaternion(x * senoAnguloSobre2, y * senoAnguloSobre2, z * senoAnguloSobre2, Math.cos(anguloSobre2));
+		return new Cuaternion(x * senoAnguloSobre2, y * senoAnguloSobre2, z
+				* senoAnguloSobre2, Math.cos(anguloSobre2));
 	}
 
-	public void setAngulo(double angulo)
-	{
+	public void setAngulo(double angulo) {
 		this.angulo = angulo;
 	}
 
-	public void setX(double x)
-	{
+	public void setX(double x) {
 		this.x = x;
 	}
 
-	public void setY(double y)
-	{
+	public void setY(double y) {
 		this.y = y;
 	}
 
-	public void setZ(double z)
-	{
+	public void setZ(double z) {
 		this.z = z;
 	}
 
-	public static Vector rotar(Rotacion rotacion, Vector vector)
-	{
+	public static Vector rotar(Rotacion rotacion, Vector vector) {
 		Cuaternion cuaternion = null;
-		if (rotacion instanceof Cuaternion)
-		{
+		if (rotacion instanceof Cuaternion) {
 			cuaternion = (Cuaternion) rotacion;
 
 		}
-		if (rotacion instanceof RotacionEsferica)
-		{
+		if (rotacion instanceof RotacionEsferica) {
 			cuaternion = ((RotacionEsferica) rotacion).aCuaternion();
 
 		}
-		return cuaternion.rotar(vector);		
-	
+		return cuaternion.rotar(vector);
+
 	}
 
-	public Cuaternion conjugado()
-	{
+	public Cuaternion conjugado() {
 		return new Cuaternion(angulo, -x, -y, -z);
 	}
 
-	public Cuaternion suma(Cuaternion b)
-	{
+	public Cuaternion suma(Cuaternion b) {
 		Cuaternion a = this;
-		return new Cuaternion(a.angulo + b.angulo, a.x + b.x, a.y + b.y, a.z + b.z);
+		return new Cuaternion(a.angulo + b.angulo, a.x + b.x, a.y + b.y, a.z
+				+ b.z);
 	}
 
-	public Cuaternion mult(Cuaternion b)
-	{
+	public Cuaternion mult(Cuaternion b) {
 		Cuaternion a = this;
 		double y0 = a.angulo * b.angulo - a.x * b.x - a.y * b.y - a.z * b.z;
 		double y1 = a.angulo * b.x + a.x * b.angulo + a.y * b.z - a.z * b.y;
@@ -145,26 +126,25 @@ public class Cuaternion implements Rotacion, Vector
 		return new Cuaternion(y0, y1, y2, y3);
 	}
 
-	public Cuaternion inverso()
-	{
+	public Cuaternion inverso() {
 		double d = angulo * angulo + x * x + y * y + z * z;
 		return new Cuaternion(angulo / d, -x / d, -y / d, -z / d);
 	}
 
-	public Cuaternion divide(Cuaternion b)
-	{
+	public Cuaternion divide(Cuaternion b) {
 		Cuaternion a = this;
 		return a.inverso().mult(b);
 	}
 
-	public double getModulo()
-	{
+	public double getModulo() {
 		return Math.sqrt(x * x + y * y + z * z);
 	}
 
-	public RotacionEsferica aRotacionEsferica()
-	{
+	public RotacionEsferica aRotacionEsferica() {
 		return new RotacionEsferica(aEsferica(), angulo);
 	}
 
+	public String toString() {
+		return angulo + " " + x + " " + y + " " + z;
+	}
 }
--- trunk/comparacion-rotaciones/src/geometria/esferica/RotacionEsferica.java
+++ trunk/comparacion-rotaciones/src/geometria/esferica/RotacionEsferica.java
@@ -4,82 +4,126 @@ import geometria.Rotacion;
 import geometria.Vector;
 import geometria.cuaterniones.Cuaternion;
 
-public class RotacionEsferica implements Rotacion
-{
+public class RotacionEsferica implements Rotacion {
 
-	private CoordenadaEsferica	eje;
-	private double				angulo;
+	private CoordenadaEsferica eje;
+	private double angulo;
 
-	public RotacionEsferica(CoordenadaEsferica eje, double angulo)
-	{
+	public RotacionEsferica(CoordenadaEsferica eje, double angulo) {
 		this.eje = eje;
 		this.angulo = angulo;
 	}
 
-	public RotacionEsferica(CoordenadaEsferica eje, double angulo, CoordenadaEsferica vector)
-	{
+	public RotacionEsferica(CoordenadaEsferica eje, double angulo,
+			CoordenadaEsferica vector) {
 		this.eje = eje;
 		this.angulo = angulo;
 	}
 
-	public Cuaternion aCuaternion()
-	{
-		Cuaternion cua=eje.aCuaternion();
+	public Cuaternion aCuaternion() {
+		Cuaternion cua = eje.aCuaternion();
 		cua.setAngulo(angulo);
 		return cua;
 	}
 
-	public double getAngulo()
-	{
+	public double getAngulo() {
 		return angulo;
 	}
 
-	public CoordenadaEsferica getEje()
-	{
+	public CoordenadaEsferica getEje() {
 		return eje;
 	}
 
 	@Override
-	public Vector rotar(Vector v)
-	{
+	public Vector rotar(Vector v) {
 		CoordenadaEsferica coordenadaEsferica = null;
-		if (v instanceof Cuaternion)
-		{
+		if (v instanceof Cuaternion) {
 			coordenadaEsferica = ((Cuaternion) v).aEsferica();
+			System.out.println("rotar esfecica dato cuaternion");
 		}
-		if (v instanceof CoordenadaEsferica)
-		{
+		if (v instanceof CoordenadaEsferica) {
 			coordenadaEsferica = (CoordenadaEsferica) v;
+			System.out.println("rotar esfecica dato esferica");
 		}
 
 		return rotar(coordenadaEsferica);
 	}
 
-	public CoordenadaEsferica rotar(CoordenadaEsferica v)
-	{
-		return v;
+	public CoordenadaEsferica rotar(CoordenadaEsferica p) {
+		// eje de la rotacion u
+		// vector a rotar p
+		CoordenadaEsferica u = eje;
+		double difLambda = p.getLambda() - u.getLambda();
+
+		double cosenoDifLambda = Math.cos(difLambda);
+		double senoDifLambda = Math.sin(difLambda);
+
+		double cosenoPhiP = Math.cos(p.getPhi());
+		double senoPhiP = Math.sin(p.getPhi());
+
+		double cosenoPhiU = Math.cos(u.getPhi());
+		double senoPhiU = Math.sin(u.getPhi());
+
+		double cosenoAngulo = Math.cos(angulo);
+		double senoAngulo = Math.sin(angulo);
+
+		// Calculation of the cosine of the arc UP (equal to UP’)
+		double cosenoUP = cosenoPhiU * cosenoPhiP + senoPhiU * senoPhiP
+				* cosenoDifLambda;
+
+		// Calculation of the sine of the arc UP. OJO, ESTO LO HACIA ABAJO
+		// seno^2(a)+coseno^2(a)=1
+		double senoUP = Math.sqrt(1 - cosenoUP * cosenoUP);
+		// double senoUP =-1*
+		// (senoPhiP*cosenoDifLambda-senoPhiU*cosenoUP)/(cosenoPUZ*cosenoPhiU);
+
+		// Calculation of the cosine of the dihedron angle PUZ
+		double cosenoPUZ = (cosenoPhiP - cosenoPhiU * cosenoUP) / senoPhiU
+				* senoUP;
+
+		// Calculation of the sine of PUZ
+		double senoPUZ = (senoUP * senoPhiP) / senoDifLambda;
+
+		// cos(P'UZ) = cos(PUZ) * cos(omega) - sin(PUZ) * sin(omega)
+		double cosenoP1UZ = (cosenoPUZ * cosenoAngulo) - (senoPUZ * senoAngulo);
+
+		// cos(phiP') = cos(up') * cos(phiU) + sin(up') * sin(phiU) * cos(P'UZ)
+		double cosenoPhiP1 = (cosenoUP * cosenoPhiU)
+				+ (senoUP * senoPhiU * cosenoP1UZ);
+
+		// cos(up) = RAIZ(1 - cos2(up))
+		double senoPhiP1 = Math.sqrt(1 - cosenoPhiP1 * cosenoPhiP1);
+
+		// cos(P'ZU) = ( cos(up) - cos(phiU) * cos(phiP') ) / sin(phiU) *
+		// sin(phiP')
+		double cosenoP1ZU = (cosenoUP - cosenoPhiU * cosenoPhiP1)
+				/ (senoPhiU * senoPhiP1);
+
+
+		// P'ZU = arc cos(cos(P'ZU))
+
+		double P1ZU = Math.acos(cosenoP1ZU);
+
+
+		return new CoordenadaEsferica(u.getLambda() + P1ZU,
+				Math.acos(cosenoPhiP1));
 	}
 
-	public void setAngulo(double angulo)
-	{
+	public void setAngulo(double angulo) {
 		this.angulo = angulo;
 	}
 
-	public void setEje(CoordenadaEsferica eje)
-	{
+	public void setEje(CoordenadaEsferica eje) {
 		this.eje = eje;
 	}
 
-	public static Vector rotar(Rotacion rotacion, Vector vector)
-	{
+	public static Vector rotar(Rotacion rotacion, Vector vector) {
 		RotacionEsferica rotacionEsferica = null;
-		if (rotacion instanceof RotacionEsferica)
-		{
+		if (rotacion instanceof RotacionEsferica) {
 			rotacionEsferica = (RotacionEsferica) rotacion;
 
 		}
-		if (rotacion instanceof Cuaternion)
-		{
+		if (rotacion instanceof Cuaternion) {
 			rotacionEsferica = ((Cuaternion) rotacion).aRotacionEsferica();
 
 		}
